f09f08ec7761fbaaee345359c9207d46058af456
Takashi Mano <webtakashi@gmail.com>
2020-01-15 09:23:21 +0900

    a

--- scraping_nijisanji.py
+++ scraping_nijisanji.py
@@ -1,58 +1,62 @@
 import os
 import pandas as pd
+from concurrent.futures import ProcessPoolExecutor
 import sqlite3
 import yaml
 
 import db.dbkey as dbkey
 from plotter.bar_plotter import BarPlotter
 from scraper.nijisanji_scraper import NijisanjiScraper
-from db.vtuber_profile_db import VTuberProfileDB
 
-USE_DUMP = True
-UNIT_TEST = False
+USE_DUMP    = False
+UNIT_TEST   = False
 
+RANK_DB_PATH        = './db/vtuber_rank.db'
+NIJISANJI_YAML_PATH   = './db/nijisanji_db.yaml'
+
+# 単体テスト用
 if UNIT_TEST:
     nj = NijisanjiScraper('月ノ美兎')
     print(nj.profile())
     import sys
     sys.exit()
 
-# pandas生成
-df = pd.read_sql_query('SELECT * FROM vtuber_rank', sqlite3.connect('./vtuber_rank.db'))
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ランキングDBを元に、ライバー情報を収集
+#
+# comment:
+#   ns['name']で全データだが、ns['name'][0:5]とすることで先頭5件のみを処理できる。
+#   [v for v in vtubers if v and v['age'] <= 100]でフィルタリングすることもできる。
+#______________________________________________________________________________
+
+# データフレーム作成
+df = pd.read_sql_query('SELECT * FROM vtuber_rank', sqlite3.connect(RANK_DB_PATH))
 
 # にじさんじのみ抽出
 ns = df[df['office'] == 'にじさんじ']
 
-# ns['name']で全データだが、ns['name'][0:5]とすることで先頭5件のみを処理できる。
-# [v for v in vtubers if v and v['age'] <= 100]でフィルタリングすることもできる。
-
-# ランキングから抽出した全にじさんじライバーの詳細情報抽出
+# 全ライバーの詳細抽出
 vtubers = []
 if not USE_DUMP:
+
+    # 詳細抽出
+    scrapers = []
     for n in ns['name']:
-        # print(n)
         vtubers.append(NijisanjiScraper(n).profile())
 
-    # 取得した情報をyamlファイルへ退避しておく。
-    with open('nijisanji_profiles.yaml', 'w') as f:
-        yaml.dump(vtubers, f, encoding='utf-8', allow_unicode=True)
-else:
-    with open('nijisanji_profiles.yaml', 'r') as f:
-        vtubers = yaml.safe_load(f)
-
-# DBに登録
-os.remove('./nijisanji_profile.db')
-db = VTuberProfileDB('./nijisanji_profile.db')
-for v in vtubers:
-    # print(v)
+    # YAML保存
     try:
-        db.insert(
-            v[dbkey.VTUBER_NAME_KEY],
-            v[dbkey.VTUBER_AGE_KEY],
-            v[dbkey.VTUBER_HEIGHT_KEY],
-            v[dbkey.VTUBER_BIRTHDAY_KEY])
-    except KeyError:
-        print('なんかおかしなデータ混ざった : {}'.format(v))
+        os.remove(NIJISANJI_YAML_PATH)
+    except FileNotFoundError:
+        pass
+    with open(NIJISANJI_YAML_PATH, 'w') as f:
+        yaml.dump(vtubers, f, encoding='utf-8', allow_unicode=True)
+
+else:
+    # YAMLから詳細ロード
+    with open(NIJISANJI_YAML_PATH, 'r') as f:
+        vtubers = yaml.safe_load(f)
 
 # データフレーム生成
 df_vtubers = pd.DataFrame.from_dict(vtubers)
@@ -61,9 +65,9 @@ df_vtubers = df_vtubers[df_vtubers['age'] != 'unknown']
 df_vtubers = df_vtubers[df_vtubers['height'] != 'unknown']
 
 # 50歳以上のライバーをdf機能で絞り込み
-print(df_vtubers[df_vtubers['age'] > 50])
+# print(df_vtubers[df_vtubers['age'] > 50])
 
-# print(df_vtubers['height'].describe())
+# 身長統計
 print(df_vtubers['height'].min())
 print(df_vtubers['height'].max())
 print(df_vtubers['height'].mean())
